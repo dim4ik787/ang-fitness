97a8dd759576ff38b616d858638df1f79cdb57cb
Dmitriy Guseinov <Diman777666@yandex.ru>
2023-06-07 21:58:25 +0400

    feat: tailwind + auth layout

--- src/auth/shared/components/auth-form/auth-form.component.ts
+++ src/auth/shared/components/auth-form/auth-form.component.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth-form',
   templateUrl: './auth-form.component.html',
   styleUrls: ['./auth-form.component.scss'],
 })
-export class AuthFormComponent {}
+export class AuthFormComponent {
+  @Output()
+  submitted = new EventEmitter<FormGroup>();
+
+  form = this.formBuilder.group({
+    email: ['', Validators.email],
+    password: ['', Validators.required],
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  onSubmit() {
+    if (this.form.valid) {
+      this.submitted.emit(this.form);
+    }
+  }
+
+  get passwordInvalid(): boolean {
+    const control = this.form.get('password')!;
+    return control.hasError('required') && control.touched;
+  }
+
+  get emailFormat(): boolean {
+    const control = this.form.get('email')!;
+    return control.hasError('email') && control.touched;
+  }
+}
